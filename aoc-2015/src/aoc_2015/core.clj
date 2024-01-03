a92b6560494b59166fe58e96410406c36c77eff2
GiorgosAthanasopoulos <spzdoom@gmail.com>
2024-01-04 01:19:33 +0200

    init

---
+++ aoc-2015/src/aoc_2015/core.clj
@@ -0,0 +1,13 @@
+(ns aoc-2015.core
+  (:gen-class))
+
+(defn day1 []
+  (let [_ (slurp "resources/1")
+        day1-1 (fn [] ())
+        day1-2 (fn [] ())]
+    (day1-1)
+    (day1-2)))
+
+(defn -main
+  [& _]
+  (day1))
