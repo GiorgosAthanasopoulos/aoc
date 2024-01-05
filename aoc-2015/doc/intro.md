439a7f645372a11b75d2996dd2e6db3590895255
GiorgosAthanasopoulos <spzdoom@gmail.com>
2024-01-05 15:00:48 +0200

    removed unneccessary files

--- aoc-2015/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to aoc-2015
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- aoc-2015/src/aoc_2015/core.clj
+++ aoc-2015/src/aoc_2015/core.clj
@@ -3,60 +3,61 @@
   (:require
    [clojure.string :as str]))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn day1 []
-  (def input (slurp "resources/1"))
-  (defn part-one []
-    (def counter (atom 0))
-    (doseq [c (seq input)]
-      (cond
-        (= c \() (swap! counter inc)
-        (= c \)) (swap! counter dec)))
-    @counter)
-  (defn part-two []
-    (def counter (atom 0))
-    (def pos (atom 0))
-    (def charnumber (atom 0))
-    (doseq [c (seq input)]
-      (cond
-        (= c \() (swap! counter inc)
-        (= c \)) (swap! counter dec))
-      (if (and (= @counter -1) (= @pos 0))
-        (reset! pos (+ @charnumber 1)))
-      (swap! charnumber inc))
-    @pos)
-  (println (part-one))
-  (println (part-two)))
+  (let [input (slurp "resources/1")
+        part-one (fn []
+                   (let [counter (atom 0)]
+                     (doseq [c (seq input)]
+                       (cond
+                         (= c \() (swap! counter inc)
+                         (= c \)) (swap! counter dec)))
+                     @counter))
+        part-two (fn []
+                   (let [counter (atom 0)
+                         pos (atom 0)
+                         charnumber (atom 0)]
+                     (doseq [c (seq input)]
+                       (cond
+                         (= c \() (swap! counter inc)
+                         (= c \)) (swap! counter dec))
+                       (when (and (= @counter -1) (= @pos 0))
+                         (reset! pos (+ @charnumber 1)))
+                       (swap! charnumber inc))
+                     @pos))]
+    (println (part-one))
+    (println (part-two))))
 
 (defn day2 []
-  (def input (slurp "resources/2"))
-  (defn split-by-x [string]
-    (str/split string #"x"))
-  (defn get-l [line]
-    (Integer. (get (split-by-x line) 0)))
-  (defn get-w [line]
-    (Integer. (get (split-by-x line) 1)))
-  (defn get-h [line]
-    (Integer. (get (split-by-x line) 2)))
-  (defn get-smallest-side [x y z]
-    (min x y z))
-  (defn part-one []
-    (def counter (atom 0))
-    (doseq [line (str/split-lines input)]
-      (def l (get-l line))
-      (def w (get-w line))
-      (def h (get-h line))
-      (def area-1 (* 2 (* l w)))
-      (def area-2 (* 2 (* w h)))
-      (def area-3 (* 2 (* h l)))
-      (def smallest-side (get-smallest-side area-1 area-2 area-3))
-      (reset! counter (+ @counter area-1))
-      (reset! counter (+ @counter area-2))
-      (reset! counter (+ @counter area-3))
-      (reset! counter (+ @counter smallest-side)))
-    @counter)
-  (defn part-two [] ())
-  (println (part-one))
-  (println (part-two)))
+  (let [input (slurp "resources/2")
+        split-by-x (fn [string]
+                     (str/split string #"x"))
+        get-l (fn [line]
+                (Integer. (get (split-by-x line) 0)))
+        get-w (fn [line]
+                (Integer. (get (split-by-x line) 1)))
+        get-h (fn [line]
+                (Integer. (get (split-by-x line) 2)))
+        get-smallest-side (fn [area-1 area-2 area-3]
+                            (min area-1 area-2 area-3))
+        part-one (fn []
+                   (let [counter (atom 0)]
+                     (doseq [line (str/split-lines input)]
+                       (let [l (get-l line)
+                             w (get-w line)
+                             h (get-h line)
+                             area-1 (* 2 (* l w))
+                             area-2 (* 2 (* w h))
+                             area-3 (* 2 (* h l))
+                             smallest-side (get-smallest-side area-1 area-2 area-3)]
+                         (reset! counter (+ @counter area-1))
+                         (reset! counter (+ @counter area-2))
+                         (reset! counter (+ @counter area-3))
+                         (reset! counter (+ @counter smallest-side))))
+                     @counter))
+        part-two (fn [] ())]
+    (println (part-one))
+    (println (part-two))))
 
 (defn -main
   [& _]
