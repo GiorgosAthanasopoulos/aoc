11f98d24fd7b551cbd1c0f776bccc2a638b5a3a5
GiorgosAthanasopoulos <spzdoom@gmail.com>
2024-01-06 18:00:31 +0200

    day 3

--- aoc-2015/src/aoc_2015/core.clj
+++ aoc-2015/src/aoc_2015/core.clj
@@ -28,37 +28,93 @@
     (println (part-one))
     (println (part-two))))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn day2 []
   (let [input (slurp "resources/2")
         split-by-x (fn [string]
                      (str/split string #"x"))
-        get-l (fn [line]
-                (Integer. (get (split-by-x line) 0)))
-        get-w (fn [line]
-                (Integer. (get (split-by-x line) 1)))
-        get-h (fn [line]
-                (Integer. (get (split-by-x line) 2)))
-        get-smallest-side (fn [area-1 area-2 area-3]
-                            (min area-1 area-2 area-3))
+        get-dim (fn [line idx]
+                  (Integer. (get (split-by-x line) idx)))
+        get-second-smallest-side (fn [l w h]
+                                   (let [sorted-dims (sort [l w h])]
+                                     (nth sorted-dims 1)))
         part-one (fn []
                    (let [counter (atom 0)]
                      (doseq [line (str/split-lines input)]
-                       (let [l (get-l line)
-                             w (get-w line)
-                             h (get-h line)
-                             area-1 (* 2 (* l w))
-                             area-2 (* 2 (* w h))
-                             area-3 (* 2 (* h l))
-                             smallest-side (get-smallest-side area-1 area-2 area-3)]
-                         (reset! counter (+ @counter area-1))
-                         (reset! counter (+ @counter area-2))
-                         (reset! counter (+ @counter area-3))
+                       (let [l (get-dim line 0)
+                             w (get-dim line 1)
+                             h (get-dim line 2)
+                             area-1 (* l w)
+                             area-2 (* w h)
+                             area-3 (* h l)
+                             smallest-side (min area-1 area-2 area-3)]
+                         (reset! counter (+ @counter (* 2 area-1)))
+                         (reset! counter (+ @counter (* 2 area-2)))
+                         (reset! counter (+ @counter (* 2 area-3)))
                          (reset! counter (+ @counter smallest-side))))
                      @counter))
-        part-two (fn [] ())]
+        part-two (fn []
+                   (let [counter (atom 0)]
+                     (doseq [line (str/split-lines input)]
+                       (let [l (get-dim line 0)
+                             w (get-dim line 1)
+                             h (get-dim line 2)]
+                         (reset! counter (+ @counter (* l w h)))
+                         (reset! counter (+ @counter (* 2 (min l w h))))
+                         (reset! counter (+ @counter (* 2 (get-second-smallest-side l w h))))))
+                     @counter))]
+    (println (part-one))
+    (println (part-two))))
+
+(defn day3 []
+  (let [input (slurp "resources/3")
+        part-one (fn []
+                   (let [counter (atom 0)
+                         x (atom 0)
+                         y (atom 0)
+                         visited (atom (set (str @x @y)))]
+                     (doseq [c (seq input)]
+                       (cond
+                         (= c \>) (swap! x inc)
+                         (= c \<) (swap! x dec)
+                         (= c \^) (swap! y inc)
+                         (= c \v) (swap! y dec))
+                       (when (not (contains? @visited (str @x @y)))
+                         (swap! counter inc)
+                         (reset! visited (conj @visited (str @x @y)))))
+                     @counter))
+        part-two (fn []
+                   (let [counter (atom 0)
+                         santa-x (atom 0)
+                         santa-y (atom 0)
+                         robot-x (atom 0)
+                         robot-y (atom 0)
+                         visited (atom (set (str @santa-x @santa-y)))
+                         idx (atom 0)]
+                     (doseq [c (seq input)]
+                       (if (= @idx 0)
+                         (do (cond
+                               (= c \>) (swap! santa-x inc)
+                               (= c \<) (swap! santa-x dec)
+                               (= c \^) (swap! santa-y inc)
+                               (= c \v) (swap! santa-y dec))
+                             (when (not (contains? @visited (str @santa-x @santa-y)))
+                               (swap! counter inc)
+                               (reset! visited (conj @visited (str @santa-x @santa-y))))
+                             (swap! idx inc))
+                         (do (cond
+                               (= c \>) (swap! robot-x inc)
+                               (= c \<) (swap! robot-x dec)
+                               (= c \^) (swap! robot-y inc)
+                               (= c \v) (swap! robot-y dec))
+                             (when (not (contains? @visited (str @robot-x @robot-y)))
+                               (swap! counter inc)
+                               (reset! visited (conj @visited (str @robot-x @robot-y))))
+                             (swap! idx dec))))
+                     @counter))]
     (println (part-one))
     (println (part-two))))
 
 (defn -main
   []
-  (day2))
+  (day3))
